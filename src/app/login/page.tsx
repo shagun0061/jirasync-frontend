a90dca341b4854e4111186073ac91f768dd6c7fd
shagun sharma <shagun.sharma@digimantra.com>
2024-10-13 23:29:53 +0530

    Login ui Done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,208 @@
-import Navbar from "@/components/Navbar";
-import { Box } from "@mui/material";
+'use client'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import TeamsLogo from "../../../public/images/teamsLogo.jpeg"
+import Checkbox from '@mui/material/Checkbox';
+import Divider from '@mui/material/Divider';
+import FormLabel from '@mui/material/FormLabel';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+// import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { styled } from '@mui/material/styles';
+import Navbar from '@/components/Navbar';
+import Image from 'next/image';
+import { IconButton, InputAdornment } from '@mui/material';
 
-export default function Login() {
-    return <Box>
-        <Navbar />
-        Login
-    </Box>
+// import ForgotPassword from './ForgotPassword';
+
+
+const Card = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    alignSelf: 'center',
+    width: '100%',
+    padding: theme.spacing(4),
+    margin: 'auto',
+    gap: theme.spacing(2),
+    boxShadow:
+        'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+    [theme.breakpoints.up('sm')]: {
+        width: '450px',
+    },
+    ...theme.applyStyles('dark', {
+        boxShadow:
+            'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+    }),
+}));
+
+export default function SignInCard() {
+    const [emailError, setEmailError] = React.useState(false);
+    const [emailErrorMessage, setEmailErrorMessage] = React.useState('');
+    const [passwordError, setPasswordError] = React.useState(false);
+    const [passwordErrorMessage, setPasswordErrorMessage] = React.useState('');
+    // const [open, setOpen] = React.useState(false);
+
+    // const handleClickOpen = () => {
+    //     setOpen(true);
+    // };
+
+    // const handleClose = () => {
+    //     setOpen(false);
+    // };
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleTogglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        if (emailError || passwordError) {
+            event.preventDefault();
+            return;
+        }
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
+    const validateInputs = () => {
+        const email = document.getElementById('email') as HTMLInputElement;
+        const password = document.getElementById('password') as HTMLInputElement;
+
+        let isValid = true;
+
+        if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
+            setEmailError(true);
+            setEmailErrorMessage('Please enter a valid email address.');
+            isValid = false;
+        } else {
+            setEmailError(false);
+            setEmailErrorMessage('');
+        }
+
+        if (!password.value || password.value.length < 6) {
+            setPasswordError(true);
+            setPasswordErrorMessage('Password must be at least 6 characters long.');
+            isValid = false;
+        } else {
+            setPasswordError(false);
+            setPasswordErrorMessage('');
+        }
+
+        return isValid;
+    };
+
+    return (
+        <Box sx={{ height: '100vh', border: '1px solid red', width: '100%', display: 'flex', flexDirection: 'column' }}>
+            <Navbar />
+            <Card>
+                <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                    {/* <SitemarkIcon /> */}
+                </Box>
+                <Typography
+                    component="h1"
+                    variant="h4"
+                    sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)', textAlign: 'center' }}
+                >
+                    Log in
+                </Typography>
+                <Box
+                    component="form"
+                    onSubmit={handleSubmit}
+                    noValidate
+                    sx={{ display: 'flex', flexDirection: 'column', width: '100%', gap: 2 }}
+                >
+                    <FormControl>
+                        <FormLabel htmlFor="email">Email</FormLabel>
+                        <TextField
+                            error={emailError}
+                            helperText={emailErrorMessage}
+                            id="email"
+                            type="email"
+                            name="email"
+                            placeholder="your@email.com"
+                            autoComplete="email"
+                            autoFocus
+                            required
+                            fullWidth
+                            variant="outlined"
+                            color={emailError ? 'error' : 'primary'}
+                            sx={{ ariaLabel: 'email' }}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <FormLabel htmlFor="password">Password</FormLabel>
+                        </Box>
+                        <TextField
+                            error={passwordError}
+                            helperText={passwordErrorMessage}
+                            name="password"
+                            placeholder="••••••"
+                            type={showPassword ? 'text' : 'password'}
+                            id="password"
+                            autoComplete="current-password"
+                            autoFocus
+                            required
+                            fullWidth
+                            variant="outlined"
+                            color={passwordError ? 'error' : 'primary'}
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleTogglePasswordVisibility}
+                                            edge="end"
+                                        >
+                                            {/* {showPassword ? <VisibilityOff /> : <Visibility />} */}
+                                        </IconButton>
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                    </FormControl>
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    />
+                    {/* <ForgotPassword open={open} handleClose={handleClose} /> */}
+                    <Button type="submit" fullWidth variant="contained" onClick={validateInputs}>
+                        Log in
+                    </Button>
+                    <Typography sx={{ textAlign: 'center' }}>
+                        Don&apos;t Remember Password?{' '}
+                        <span>
+                            <Link
+                                component="button"
+                                type="button"
+                                // onClick={handleClickOpen}
+                                variant="body2"
+                                sx={{ alignSelf: 'baseline' }}
+                            >
+                                Forgot your password?
+                            </Link>
+                        </span>
+                    </Typography>
+                </Box>
+                <Divider>or</Divider>
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+                    <Button
+                        fullWidth
+                        variant="outlined"
+                        onClick={() => alert('Log in with Teams')}
+                        startIcon={<Image src={TeamsLogo} alt="Teams Logo" width={30} height={30} />}
+                    >
+                        Log in with Teams
+                    </Button>
+                </Box>
+            </Card>
+        </Box>
+    );
 }
